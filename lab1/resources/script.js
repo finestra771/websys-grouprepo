f2141d88d13c4850aae79c703f3b8c307ebe1115
Dathan Lang <dathan_lang@outlook.com>
2025-09-19 16:59:08 -0400

    animation for href links

--- lab1/resources/script.js
+++ lab1/resources/script.js
@@ -11,3 +11,34 @@ window.addEventListener("scrollend", (event) => {
     const navbar = document.querySelector("nav");
     navbar.classList.remove('nav-dimmed');
 });
+
+
+$(document).ready(() => {
+    $('#link1').click(function (e) {
+        e.preventDefault(); 
+        $('html, body').animate({
+            scrollTop: $('#home').offset().top
+        }, 700);
+    });
+
+    $('#link2').click(function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $('#gallery').offset().top
+        }, 700);
+    });
+
+    $('#link3').click(function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $('#about-us').offset().top
+        }, 700);
+    });
+
+    $('#link4').click(function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $('#contacts').offset().top
+        }, 700);
+    });
+});
