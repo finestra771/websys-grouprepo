8ec3bd7446b14374b1a3aa613a35c283bb8f1d69
Jodie <minjokwindows5@gmail.com>
2025-11-07 14:41:35 -0500

    quotes

--- Previous labs/lab7/init.sql
+++ Previous labs/lab7/init.sql
@@ -1,27 +1,28 @@
 CREATE DATABASE IF NOT EXISTS `lab7`;
+USE `lab7`;
 
 CREATE table If NOT EXISTS `lab7`.`courses`(
-    crn INT(11) PRIMARY KEY,
-    prefix VARCHAR(4) NOT NULL,
-    number SMALLINT(4) NOT NULL,
-    title VARCHAR(255) NOT NULL
+    `crn` INT(11) PRIMARY KEY,
+    `prefix` VARCHAR(4) NOT NULL,
+    `number` SMALLINT(4) NOT NULL,
+    `title` VARCHAR(255) NOT NULL
 );
 
 CREATE table IF not exists `lab7`.`students`(
-    RIN int(9) PRIMARY Key,
-    RCSID char(7),
-    first_name varchar(100) NOT NULL,
-    last_name varchar(100) NOT NULL,
-    alias varchar(100) NOT NULL,
-    phone int(10)
+    `RIN` int(9) PRIMARY Key,
+    `RCSID` char(7),
+    `first_name` varchar(100) NOT NULL,
+    `last_name` varchar(100) NOT NULL,
+    `alias` varchar(100) NOT NULL,
+    `phone` int(10)
 );
 
 ALTER TABLE `lab7`.`students`
-ADD street VARCHAR(255),
-ADD city VARCHAR(100),
-ADD state CHAR(2),
-ADD zip CHAR(10);
+ADD `street` VARCHAR(255),
+ADD `city` VARCHAR(100),
+ADD `state` CHAR(2),
+ADD `zip` CHAR(10);
 
 ALTER TABLE `lab7`.`courses`
-ADD section INT(4) NOT NULL,
-ADD year VARCHAR(6) NOT NULL;
+ADD `section` INT(4) NOT NULL,
+ADD `year` VARCHAR(6) NOT NULL;
