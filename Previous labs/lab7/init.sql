01497618e4d892ee0ef24eb96db1bf2ebb9a69e8
Jodie <minjokwindows5@gmail.com>
2025-11-07 14:38:05 -0500

    minor change

--- Previous labs/lab7/init.sql
+++ Previous labs/lab7/init.sql
@@ -1,13 +1,13 @@
-CREATE DATABASE IF NOT EXISTS lab7;
+CREATE DATABASE IF NOT EXISTS `lab7`;
 
-CREATE table If NOT EXISTS lab7.courses(
+CREATE table If NOT EXISTS `lab7`.`courses`(
     crn INT(11) PRIMARY KEY,
     prefix VARCHAR(4) NOT NULL,
     number SMALLINT(4) NOT NULL,
     title VARCHAR(255) NOT NULL
 );
 
-CREATE table IF not exists lab7.students(
+CREATE table IF not exists `lab7`.`students`(
     RIN int(9) PRIMARY Key,
     RCSID char(7),
     first_name varchar(100) NOT NULL,
@@ -16,12 +16,12 @@ CREATE table IF not exists lab7.students(
     phone int(10)
 );
 
-ALTER TABLE lab7.students
+ALTER TABLE `lab7`.`students`
 ADD street VARCHAR(255),
 ADD city VARCHAR(100),
 ADD state CHAR(2),
 ADD zip CHAR(10);
 
-ALTER TABLE lab7.courses
+ALTER TABLE `lab7`.`courses`
 ADD section INT(4) NOT NULL,
 ADD year VARCHAR(6) NOT NULL;
