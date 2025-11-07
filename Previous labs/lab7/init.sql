a2bdbf18346da5d28dcf50d62070334a2c75097c
crotto8404 <crotto@rpi.edu>
2025-11-07 15:06:58 -0500

    Update init.sql

--- Previous labs/lab7/init.sql
+++ Previous labs/lab7/init.sql
@@ -26,3 +26,17 @@ ADD `zip` CHAR(10);
 ALTER TABLE `lab7`.`courses`
 ADD `section` INT(4) NOT NULL,
 ADD `year` VARCHAR(6) NOT NULL;
+
+INSERT INTO 'lab7'.'courses'
+VALUES (73048, "ITWS", 2110, "Web Systems Development", 01, 2025),
+    (36138, "ITWS", 4500, "Web Science Systems Dev", 01, 2026),
+    (35258, "CSCI", 1200, "Data Structures", 02, 2026),
+    (35492, "CSCI", 2300, "Introduction To Algorithms", 01, 2026);
+
+INSERT INTO `lab7`.'students'
+VALUES (661234567, 'smithj', 'John', 'Smith', 'Johnny', 5185551234, '12 Oak Street', 'Albany', 'NY', '12203'),
+    (661234568, 'garciam', 'Maria', 'Garcia', 'Ria', 5185552345, '24 Pine Avenue', 'Troy', 'NY', '12180'),
+    (661234569, 'browna', 'Alex', 'Brown', 'Lex', 5185553456, '56 Maple Lane', 'Schenectady', 'NY', '12308'),
+    (661234570, 'wilsonl', 'Liam', 'Wilson', 'Lee', 5185554567, '89 Elm Road', 'Saratoga', 'NY', '12866');
+
+    
